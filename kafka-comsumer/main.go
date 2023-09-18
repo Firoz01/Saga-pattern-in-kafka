b7421fbe134b2c4bde23f2fc64f7a282a77cf4cf
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2023-09-18 16:31:45 +0600

    kafka

--- kafka-comsumer/main.go
+++ kafka-comsumer/main.go
@@ -3,35 +3,119 @@ package main
 import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"os"
 )
 
+var producer *kafka.Producer
+var consumer *kafka.Consumer
+
 func main() {
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "192.168.0.169:9092",
-		"group.id":          "foo",
-		"auto.offset.reset": "smallest"})
+	// Initialize the Kafka producer
+	initKafkaProducer()
+	initKafkaConsumer()
+	// Create an Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	//e.POST("/publish", publishMessage)
+
+	// Start the server
+	e.Start(":8081")
+}
+
+func initKafkaProducer() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "192.168.0.169:9092", // Specify the correct Kafka broker address
+		"client.id":         getHostname(),
+	})
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Failed to create Kafka producer: %s\n", err)
+		os.Exit(1)
 	}
 
-	topics := []string{"kafka-test"}
-	err = consumer.SubscribeTopics(topics, nil)
+	producer = p
 
-	for {
-		ev := consumer.Poll(100)
-		switch e := ev.(type) {
-		case *kafka.Message:
-			if e.Value != nil {
-				fmt.Printf("Consumed message: %s\n", string(e.Value))
+	go func() {
+		for e := range producer.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
+				}
+			default:
+				fmt.Printf("Ignored event: %v\n", ev)
 			}
-		case kafka.Error:
-			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
-		default:
-			fmt.Printf("Ignored %v\n", e)
 		}
+	}()
+}
+
+func initKafkaConsumer() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  "192.168.0.169:9092",
+		"group.id":           "my-group", // Specify your consumer group ID
+		"auto.offset.reset":  "earliest",
+		"enable.auto.commit": "false",
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to create Kafka consumer: %s\n", err)
+		os.Exit(1)
 	}
 
-	consumer.Close()
+	consumer = c
+
+	err = consumer.SubscribeTopics([]string{"kafka-one"}, nil)
+	if err != nil {
+		fmt.Printf("Error subscribing to topic: %v\n", err)
+		os.Exit(1)
+	}
+
+	go func() {
+		for {
+			msg, err := consumer.ReadMessage(-1)
+			if err == nil {
+				fmt.Printf("Consumed message from Kafka: %s\n", string(msg.Value))
+				// Process the returned message here
+				publishMessage(string(msg.Value), "kafka-two")
+			} else {
+				fmt.Printf("Error reading message from Kafka: %v\n", err)
+			}
+		}
+	}()
+}
+
+func publishMessage(message string, topic string) error {
+	//topic := "kafka-two"
+
+	modifyMessage := message + " modified"
+
+	err := producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(modifyMessage),
+	}, nil)
+
+	if err != nil {
+		fmt.Println("failed to produced message from server two")
+	}
+	return err
+}
+
+func getHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Printf("Error getting hostname: %s\n", err)
+		return "default-client-id"
+	}
+	fmt.Println(hostname)
+	return hostname
 }
--- kafka-producer/main.go
+++ kafka-producer/main.go
@@ -1,57 +1,125 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	"os"
 )
 
+var producer *kafka.Producer
+var consumer *kafka.Consumer
+
 func main() {
+	// Create an Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/publish/transaction", publishMessageInTransaction)
+
+	// Initialize Kafka producer and transactions
+	initKafkaProducerAndTransactions()
+	initKafkaConsumer()
+	// Start the server
+	e.Start(":8080")
+}
+
+func initKafkaProducerAndTransactions() {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": "192.168.0.169:9092", // Specify the correct Kafka broker address
-		"client.id":         getHostname(),
+		"bootstrap.servers":  "192.168.0.169:9092",
+		"client.id":          getHostname(),
+		"transactional.id":   "my-transactional-id",
+		"acks":               "all",
+		"enable.idempotence": "true",
 	})
 
 	if err != nil {
-		fmt.Printf("Failed to create producer: %s\n", err)
+		fmt.Printf("Failed to create Kafka producer: %s\n", err)
 		os.Exit(1)
 	}
 
-	defer p.Close() // Make sure to close the producer when done
+	// Initialize Kafka transactions
+	ctx := context.Background()
+	err = p.InitTransactions(ctx)
+	if err != nil {
+		fmt.Printf("Failed to initialize transactions: %s\n", err)
+		os.Exit(1)
+	}
 
-	deliveryChan := make(chan kafka.Event)
+	producer = p
+}
+
+func publishMessageInTransaction(c echo.Context) error {
+	topic := "kafka-one"
+	value := c.FormValue("message")
+
+	//ctx := c.Request().Context()
+
+	// Begin a new transaction
+	err := producer.BeginTransaction()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Failed to begin transaction: %v\n", err))
+	}
+
+	// Produce the message within the transaction
+	err = producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic,             : kafka.PartitionAny},
+		Value:          []byte(value),
+	}, nil)
+
+	if err != nil {
+		// Abort the transaction if producing the message fails
+		producer.AbortTransaction(nil)
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Failed to produce message within transaction: %v\n", err))
+	}
+
+	// Commit the transaction
+	err = producer.CommitTransaction(nil)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Failed to commit transaction: %v\n", err))
+	}
+
+	return c.String(http.StatusOK, "Message published to Kafka within a transaction")
+}
+
+func initKafkaConsumer() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  "192.168.0.169:9092",
+		"group.id":           "my-group", // Specify your consumer group ID
+		"auto.offset.reset":  "earliest",
+		"enable.auto.commit": "false",
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to create Kafka consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	consumer = c
+
+	err = consumer.SubscribeTopics([]string{"kafka-two"}, nil)
+	if err != nil {
+		fmt.Printf("Error subscribing to topic: %v\n", err)
+		os.Exit(1)
+	}
 
 	go func() {
-		for e := range deliveryChan {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition.Error)
-				} else {
-					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-						*ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
-				}
-			default:
-				fmt.Printf("Ignored event: %v\n", ev)
+		for {
+			msg, err := consumer.ReadMessage(-1)
+			if err == nil {
+				fmt.Printf("Consumed message from Kafka: %s\n", string(msg.Value))
+			} else {
+				fmt.Printf("Error reading message from Kafka: %v\n", err)
 			}
 		}
 	}()
-
-	topic := "kafka-test"
-	value := "kafka hello"
-
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value:          []byte(value),
-	}, deliveryChan)
-
-	if err != nil {
-		fmt.Printf("Failed to produce message: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Wait for the delivery report to be received
-	<-deliveryChan
 }
 
 func getHostname() string {
@@ -60,6 +128,5 @@ func getHostname() string {
 		fmt.Printf("Error getting hostname: %s\n", err)
 		return "default-client-id"
 	}
-	fmt.Println(hostname)
 	return hostname
 }
